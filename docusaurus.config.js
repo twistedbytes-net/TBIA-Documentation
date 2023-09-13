6ccbe9bfaf7760ca20d7b42d35b09135276b94eb
Andreas Oehlke <andreas.oehlke@twistedbytes.net>
2023-09-13 14:34:21 +0200

    1.6.0

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,6 +56,9 @@ const config = {
                     showLastUpdateTime: false,
                     includeCurrentVersion: false,
                     versions: {
+                        "1.6.0": {
+                            banner: "none",
+                        },
                         "1.5.2": {
                             banner: "none",
                         },
