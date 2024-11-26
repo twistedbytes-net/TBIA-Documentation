d14ab57a3c7f4e3be2df4a99f86671e1396a21fc
Andreas Oehlke <andreas.oehlke@twistedbytes.net>
2024-11-26 12:28:18 +0100

    #128

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,6 +56,9 @@ const config = {
                     showLastUpdateTime: false,
                     includeCurrentVersion: false,
                     versions: {
+                        "1.8.0": {
+                            banner: "none",
+                        },
                         "1.7.0": {
                             banner: "none",
                         },
---
+++ versioned_docs/version-1.8.0/advanced-setups/_player-state.mdx
@@ -0,0 +1,6 @@
+---
+sidebar_position: 1
+title: Using Player States
+---
+
+Using `APlayerState` to ...
